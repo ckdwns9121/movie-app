cb58c1bfe723e362f38aabc6006733a172b952d2
ckdwns9121 <ckdwns9121@naver.com>
2021-09-08 18:11:56 +0900

    Updates

---
+++ precache-manifest.b5194f53c8d2d89c9897e50a20dbd7f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a63808e42bb47d3090cad2fad50020",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "7a0cc9eaaf412907d601",
+    "url": "/movie-app/static/css/main.4a34e8d1.chunk.css"
+  },
+  {
+    "revision": "00708fb33aae39c42fc0",
+    "url": "/movie-app/static/js/2.11ae4c7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.11ae4c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0cc9eaaf412907d601",
+    "url": "/movie-app/static/js/main.635867bb.chunk.js"
+  },
+  {
+    "revision": "7e869a2352de7a9f3c65",
+    "url": "/movie-app/static/js/runtime-main.85ad0f9f.js"
+  }
+]);
